22dd0f50319c1eaaefcdd3438d26c83d166c2fe9
Leandro Wagner <leanw1711@gmail.com>
2022-03-11 10:37:48 -0300

    cambios footer

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <BrowserRouter>
             <NavBar/>
             <Routes>
-              <Route path='/mercado-shop' element={<ItemListContainer/>}/>
+              <Route path='/' element={<ItemListContainer/>}/>
               <Route path='/item/:id' element ={<ItemDetailContainer/>}/>
               <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
               <Route element={<PrivateRoute/>}>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,36 @@ import '../assets/css/Footer.css';
 import logo from '../assets/image/menu.png';  // importar logo de la web desde assets imagenes
 import CopyrightIcon from '@mui/icons-material/Copyright';
 import {NavLink,Link} from 'react-router-dom';
+import {useContext} from 'react';
+import {UserContext} from './UserContext'; 
+import {useState,useEffect} from 'react';
+
 export const Footer =()=>{
+    const user = useContext(UserContext);
+    const [fecha,setFecha]=useState();
+    
+    useEffect(()=>{
+       let hoy = new Date();
+       let dia= hoy.toLocaleDateString();
+       setFecha(dia);
+    },[])
+
     return(
         <footer className="container-fluid section-footer">
             <div className="row pt-5">
                 <div className="col-md-12 text-center">
-                    <Link to='/mercado-shop' className="footer-title-link"><p><span><img src={logo}alt="logo mercado"width="40"className="footer-title-img me-3"/></span><span className="footer-title">Mercado - Shop</span></p></Link>
+                    <Link to='/mercado-shop' className="footer-title-link"><p><span><img src={logo}alt="logo mercado"width="40"className="footer-title-img me-3"/></span><span className="footer-title">Mercado Shop</span></p></Link>
                 </div>
             </div>     
-            <div className="row container-fluid">
+            <div className="row">
                 <div className="col-md-12 text-center">
                     <ul className="footer-nav pt-3">
                         <li className="nav-item"><NavLink to='/category/novedades'className="nav-link">Novedades</NavLink></li>
                         <li className="nav-item"><NavLink to='/category/ofertas' className="nav-link">Ofertas</NavLink></li>
-                        <li className="nav-item"><Link to='/login' className="nav-link">Login</Link></li>
+                        {
+                            user.activeLogin ? <li className="nav-link disabled item-fecha">{fecha}</li>
+                            :(<li className="nav-item"><Link to='/login' className="nav-link item-fecha">Login</Link></li>)
+                        }
                     </ul>
                 </div>
             </div>
